fa0c04c8a0e729b5fbd0e016a86424c757e4937d
Mr.yao <lily02@rd.netease.com>
2022-08-29 11:09:42 +0800

    update

---
+++ 8.26/1.js
@@ -0,0 +1,8 @@
+// for 循环 从 0 开始 到 5 不包括5 
+for(let i = 0; i < 5; i++) {
+    console.log('lly-log -- i --->', i);
+}
+// for 循环 从 0 开始 到 5 不包括5 
+for(let i = 0; i <= 5; i++) {
+    console.log('lly-log -- i --->', i);
+}
